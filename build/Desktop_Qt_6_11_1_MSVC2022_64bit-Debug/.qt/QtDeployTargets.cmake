2593ea15806cc765802431cd0fea89af4517d270
Hash3816 <agafoncevdima962@gmail.com>
2026-06-14 16:36:54 +1000

    Удалил старые билды

--- build/Desktop_Qt_6_11_1_MSVC2022_64bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_course_FILE D:/ACID/dz_4/course/build/Desktop_Qt_6_11_1_MSVC2022_64bit-Debug/course.exe)
-set(__QT_DEPLOY_TARGET_course_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_course_RUNTIME_DLLS D:/Qt/6.11.1/msvc2022_64/bin/Qt6Widgetsd.dll;D:/Qt/6.11.1/msvc2022_64/bin/Qt6Guid.dll;D:/Qt/6.11.1/msvc2022_64/bin/Qt6Cored.dll)
